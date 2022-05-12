b713baab3778a9db4654f2d4a816d498a6546668
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-12 21:06:07 +0300

    ft:database configuration - links app to database

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from . import main
 
-
-
-
-
 @main.route('/')
 def index():
+    
+    return render_template('index.html')
+
+@main.route('/user')
+def user():
     
     return render_template('index.html')
