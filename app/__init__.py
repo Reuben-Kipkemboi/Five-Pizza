89be5c9cbc19bf87756d32adbd9871398784f203
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-13 07:05:50 +0300

    docs:add email.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,8 @@
 from flask import Flask 
 from config import config_options
-# from flask_mail import Mail
-# from flask_login import LoginManager
-# from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_uploads import IMAGES, UploadSet,configure_uploads
+from flask_mail import Mail
 
-# db = SQLAlchemy()
-# mail = Mail()
-# bootstap = Bootstrap()
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
-
-
-
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -25,11 +13,8 @@ def create_app(config_name):
     
     app.register_blueprint(main_blueprint)
 
-    # login_manager.init_app(app)
-    # db.init_app(app)
-    # bootstap.init_app(app)
-    # # configure_uploads(app,photos)
-    # mail.init_app(app)
+   
+    mail.init_app(app)
     
 
 
